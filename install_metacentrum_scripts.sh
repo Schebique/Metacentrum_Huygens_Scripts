cb2a3596afccae49d7bf1aea7bdae20f3695a32d
Ondrej Sebesta <sebesta@natur.cuni.cz>
2019-05-03 14:09:13 +0200

    Update install_metacentrum_scripts.sh

--- install_metacentrum_scripts.sh
+++ install_metacentrum_scripts.sh
@@ -3,19 +3,28 @@ cd $HOME
 #test initHuygens
 echo -e "would you like to reinstall all scripts? (y/n) \c"
 	read
+	if ! [ -f ./initDeconvolution.sh ] || [ "$REPLY" = "y" ]; then
+		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/initDeconvolution.sh?raw=true  -O initDeconvolution.sh -q
+		chmod 700 initDeconvolution.sh
+	fi
 	if ! [ -f ./initHuygens.sh ] || [ "$REPLY" = "y" ]; then
-		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/initHuygens.sh --unlink -O initHuygens.sh 
+		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/initHuygens.sh?raw=true  -O initHuygens.sh -q
 		chmod 700 initHuygens.sh
+		echo "initHuygens.sh installed"
 	fi
 	if ! [ -f ./initGui.sh ] || [ "$REPLY" = "y" ]; then
-		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/initGui.sh --unlink -O initGui.sh 
+		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/initGui.sh?raw=true --unlink -O initGui.sh -q
 		chmod 700 initGui.sh
+		echo "initGui.sh installed"
 	fi
-	if ! [ -f ./hupro.sh ]|| [ "$REPLY" = "y" ]; then
-		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/hupro.sh --unlink -O hupro.sh
+	if ! [ -f ./hupro.sh ] || [ "$REPLY" = "y" ]; then
+		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/hupro.sh?raw=true --unlink -O hupro.sh -q
 		chmod 700 hupro.sh
+		echo "hupro.sh installed"
+	fi
+	if ! [ -f ./How_to_use_it.txt ] || [ "$REPLY" = "y" ]; then
+		wget https://github.com/Schebique/Metacentrum_Huygens_Scripts/blob/master/How_to_use_it.txt?raw=true --unlink -O How_to_use_it.txt -q
+		cat How_to_use_it.txt
 	fi
 	if [ "$REPLY" = "n" ]; then echo "No files were installed."
 	fi
-	
-	
