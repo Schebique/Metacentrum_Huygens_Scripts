9d691d958181cbf80922624a481be86e5d6a787d
Ondrej Sebesta <sebesta@natur.cuni.cz>
2019-05-10 10:29:31 +0200

    Update initGui.sh
    user info added into VNClogin.txt

--- initGui.sh
+++ initGui.sh
@@ -5,3 +5,5 @@ path=$(pwd)/VNClogin.txt
 echo $path
 gui start -g 1920x1080 --ssh -f > $path
 #cat $path
+echo "VNC login credentials can be displayed with this command as well:" >> $path
+echo "cat VNClogin.txt" >> $path
