1a866f4a21d4aaf448af6ddfc6bdda4c68420030
Ondrej Sebesta <sebesta@natur.cuni.cz>
2019-05-10 17:16:19 +0200

    Update initGui.sh

--- initGui.sh
+++ initGui.sh
@@ -2,8 +2,6 @@
 
 module add gui
 path=$(pwd)/VNClogin.txt
-echo $path
-gui start -g 1920x1080 --ssh -f > $path
-#cat $path
-echo "VNC login credentials can be displayed with this command as well:" >> $path
-echo "cat VNClogin.txt" >> $path
+echo "use cat $path to see connection details again" > $path
+gui start -g 1920x1080 --ssh -f >> $path
+
